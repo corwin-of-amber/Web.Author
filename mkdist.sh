d2b58618c3508471b0b5e4d4f41d9ce10035c64c
Shachar Itzhaky <corwin.amber@gmail.com>
2019-09-05 23:06:00 +0300

    [build] Production-mode distribution.

---
+++ mkdist.sh
@@ -0,0 +1,15 @@
+mkdir -p dist
+
+# External packages
+cp node_modules/jquery/dist/jquery.min.js dist/
+
+mkdir -p dist/codemirror
+cp node_modules/codemirror/lib/codemirror.* \
+   node_modules/codemirror/mode/stex/stex.js dist/codemirror/
+
+cp node_modules/split.js/dist/split.js dist/
+
+cp node_modules/pdfjs-dist/build/*.min.js dist/
+
+# Sources
+cp src/browser.bundle.js src/**/*.css dist/
