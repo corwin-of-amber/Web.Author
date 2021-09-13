9421243e86638d6550359bac7e6f9fb176848fe6
Shachar Itzhaky <corwin.amber@gmail.com>
2021-09-13 19:01:21 +0300

    [misc] Tidy up `index.html`.

--- etc/vercel-build.sh
+++ etc/vercel-build.sh
@@ -3,4 +3,4 @@
 BASE_TAG='<base href="build\/kremlin\/index.html">'
 
 [ -e dist/vendor.tar.gz ] && tar xzvf dist/vendor.tar.gz
-sed "s/<!-- base -->/$BASE_TAG/" build/kremlin/index.html > index.html
+sed "s/<!-- base .* -->/$BASE_TAG/" build/kremlin/index.html > index.html
