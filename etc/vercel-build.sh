7683e765eb058fbf2d6ad8b19b6460fe3c608ad3
Shachar Itzhaky <corwin.amber@gmail.com>
2021-09-17 17:36:30 +0300

    [deploy] Serve tlnet archives from Vercel deployment.

--- etc/vercel-build.sh
+++ etc/vercel-build.sh
@@ -2,5 +2,36 @@
 
 BASE_TAG='<base href="build\/kremlin\/index.html">'
 
-[ -e dist/vendor.tar.gz ] && tar xzvf dist/vendor.tar.gz
+TLNET=https://ctan.math.illinois.edu/systems/texlive/tlnet
+DL=bin/tlnet
+DLCACHE=node_modules/vendor-cache
+
 sed "s/<!-- base .* -->/$BASE_TAG/" build/kremlin/index.html > index.html
+
+[ -e dist/vendor.tar.gz ] && tar xzvf dist/vendor.tar.gz
+
+mkdir -p $DL
+
+# Check for downloaded packages in build cache
+if [ -d $DLCACHE/ ] ; then
+    echo Vendor cache found:
+    du -hs $DLCACHE
+    cp -r $DLCACHE/* $DL/
+else
+    echo Vendor cache not found.
+fi
+
+# Download packages from tlnet
+for pkg in `cat etc/tlnet-deploy.list`; do
+    echo "$TLNET/archive/$pkg.tar.xz"
+    [ -e "$DL/$pkg.tar.xz" ] || curl "$TLNET/archive/$pkg.tar.xz" > "$DL/$pkg.tar.xz"
+done
+
+# Update build cache
+mkdir -p $DLCACHE
+cp -r $DL/* $DLCACHE/
+
+# These packages are too large to be unpacked on the client :/
+for pkg in amsfonts lm; do
+    ./dist/xzminidec < "$DL/$pkg.tar.xz" > "$DL/$pkg.tar"
+done
